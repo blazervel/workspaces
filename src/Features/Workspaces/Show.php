8066efbc849146b86b0ea3ffc62f1a2278a2eeee
Joshua Anderton <info@joshanderton.com>
2022-08-29 15:06:09 -0700

    Fix workspace show route

--- src/Features/Workspaces/Show.php
+++ src/Features/Workspaces/Show.php
@@ -3,8 +3,8 @@
 namespace Blazervel\Workspaces\Features\Workspaces;
 
 use App\Models\Workspace;
-use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\{ Request, RedirectResponse };
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Blazervel\Feature\Action;
 
 class Show extends Action
@@ -12,11 +12,16 @@ class Show extends Action
 
   public function handle(Request $request, Workspace $workspace): RedirectResponse
   {
+    $redirect = RouteServiceProvider::HOME;
     $request->user()->setCurrentWorkspace(
       workspace: $workspace
     );
 
-    return redirect()->route('scorecards.index');
+    if (class_exists($routeServiceProvider = 'App\Providers\RouteServiceProvider')) :
+      $redirect = $routeServiceProvider::HOME;
+    endif;
+
+    return redirect()->to($redirect);
   }
 
 }
