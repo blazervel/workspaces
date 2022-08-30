3962bb8e6a3a670072f33e235cb7b73029c01881
Joshua Anderton <info@joshanderton.com>
2022-08-29 18:13:43 -0700

    Fix

--- src/Features/Workspaces/Show.php
+++ src/Features/Workspaces/Show.php
@@ -4,15 +4,14 @@ namespace Blazervel\Workspaces\Features\Workspaces;
 
 use App\Models\Workspace;
 use Illuminate\Http\{ Request, RedirectResponse };
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Blazervel\Feature\Action;
 
 class Show extends Action
 {
-
   public function handle(Request $request, Workspace $workspace): RedirectResponse
   {
-    $redirect = RouteServiceProvider::HOME;
+    $redirect = '/';
+
     $request->user()->setCurrentWorkspace(
       workspace: $workspace
     );
