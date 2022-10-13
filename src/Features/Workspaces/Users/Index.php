2e6a4aa2a9cae17a70360ace6021cb8574323210
Joshua Anderton <info@joshanderton.com>
2022-10-13 14:50:11 -0700

    Cleanup

--- src/Features/Workspaces/Users/Index.php
+++ src/Features/Workspaces/Users/Index.php
@@ -4,16 +4,17 @@ namespace Blazervel\Workspaces\Features\Workspaces\Users;
 
 use App\Models\Workspace;
 use Blazervel\Features\Action;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class Index extends Action
 {
-    public function handle(Workspace $workspace): Response
+    public function handle(Request $request, Workspace $workspace): Response
     {
         return Inertia::render('@blazervel/workspaces/Pages/Users/Index', [
             'workspace' => $workspace,
-            'users' => $workspace->users()->get(),
+            'users' => $workspace->users()->where('id', '!=', $request->user()->id)->get(),
         ]);
     }
 }
